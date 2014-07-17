27e9d9d3685a9e2ea8748bac4a78ece11aab3af5
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2014-07-17 13:02:39 -0700

    tode client build sequence and script look pretty good

---
+++ bin/tode.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#=========================================================================
+# Copyright (c) 2014 GemTalk Systems, LLC <dhenrich@gemtalksystems.com>.
+#
+# Name - tode.sh 
+#
+# Purpose - launch todeClient image
+#
+#=========================================================================
+
+if [ "${GS_HOME}x" = "x" ] ; then
+  echo "the GS_HOME environment variable needs to be defined"
+  exit 1
+fi
+if [ ! -e "$GS_HOME/pharo/pharo" ]; then
+  $GS_HOME/bin/installPharo.sh
+fi
+
+# Run script
+pharo=$GS_HOME/pharo
+$pharo/pharo-ui $pharo/todeClient.image &
+
+# End of script
+exit 0
