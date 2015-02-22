946daec416b933b2a45781ce752c4683596305a7
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2015-02-22 14:33:40 -0800

    Issue #58: fix project tests

--- tests/projectTests.sh
+++ tests/projectTests.sh
@@ -9,8 +9,14 @@ $GS_HOME/bin/installGemStone $GS_VERSION
 
 $GS_HOME/bin/createTodeImage
 
-$GS_HOME/bin/createTodeProjectStone projects $GS_VERSION Flow
+$GS_HOME/bin/createTodeProjectStone projects $GS_VERSION crypto
 
-$GS_HOME/bin/installTodeProjectStone projects $GS_VERSION SIXX
+$GS_HOME/bin/installTodeProjectStone projects SIXX
+
+$GS_HOME/bin/installTodeProjectStone projects NewJSON
+
+$GS_HOME/bin/installTodeProjectStone projects Mapless
+
+$GS_HOME/bin/installTodeProjectStone projects Flow
 
 stopStone projects
