05d6c1903190aa2d26b75f52f78a9f36475d79e5
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2015-02-22 14:05:20 -0800

    Issue #58: clean up magritee and projects test scripts

--- tests/magritteTests.sh
+++ tests/magritteTests.sh
@@ -5,6 +5,10 @@
 
 set -e  # exit on error
 
+$GS_HOME/bin/installGemStone $GS_VERSION
+
+$GS_HOME/bin/createTodeImage
+
 $GS_HOME/projects/magritte3/bin/createMagritteStone magritte  $GS_VERSION
 stones
 stopStone magritte
--- tests/projectTests.sh
+++ tests/projectTests.sh
@@ -5,6 +5,12 @@
 
 set -e  # exit on error
 
+$GS_HOME/bin/installGemStone $GS_VERSION
+
+$GS_HOME/bin/createTodeImage
+
 $GS_HOME/bin/createTodeProjectStone projects $GS_VERSION Flow
+
 $GS_HOME/bin/installTodeProjectStone projects $GS_VERSION SIXX
+
 stopStone projects
